e6b505d9c932119b679cacd85d211d58c38d5810
Carlos Henrique Soares Silva <87345469+CarlosHenriqueSoaresSIlva@users.noreply.github.com>
2021-07-25 23:13:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+     const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server inicializado na porta ' + port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
